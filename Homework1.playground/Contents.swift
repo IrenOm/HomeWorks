bd0a1f5400e9465940509cc48cc8ea12742f0772
Irena Omelana <124605706+IrenOm@users.noreply.github.com>
2023-03-24 10:59:56 +0200

    Initial commit

---
+++ Homework1.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+var greeting = "Hello, playground"
+/*
+Exercise 1
+Declare two string constants firstString and secondString. Assign them the values "I'm learing" and "swift". Print the phrase "I'm learing swift !!!" using these string variables.
+*/
+
+var firstString: String = "I'm learing"
+var secondString:
+
+/*
+Exercise 2.1
+Create a constant named myAge and assign it the value of your age.
+*/
+
+/*
+Exercise 2.2
+Declare an Int variable named myAgeInTenYears and assign it the value of your age after 10 years(use the constant myAge and the addition(+) operator).
+*/
+
+
+/*
+Exercise 2.3
+Declare another daysInYear constant and set it to 365.25 (the number of days in a leap year).
+*/
+
+/*
+Exercise 2.4
+Declare a Float variable named daysPassed and set it to the number of days since you were born plus 10 years (use myAgeInTenYears(variable) and daysInYear(constant)).
+*/
+
+/*
+Exercise 2.5
+Using the print() function, print the phrases like: "My age is <...> years. After 10 years, I will be <...> years old. From the moment of my birth day have passed approximately <...>" . (Symbols (<...>) must be replaced with variable values !)
+*/
+
+/*
+Exercise 3.
+ 
+Calculate triangle's area and perimeter.
+Conditions: triangle length: AC = 8.0, CB = 6.0. The hypotenuse of triangle AB is calculated using the sqrt(Double) function, replacing Double with the sum of squares of length.
+ Steps:
+ 1.Right-angled triangle
+ 2.Hypotenuse of the triangle
+ 3.Area of a triangle
+ 4.Triangle perimeter
+*/
+
+/*
+ Ex5:
+ Calculate the number of years, months, days from you birthday to current date.
+ print("Total years: \(totalYearsFromBirth) , total months: \(totalMonthFromBirth), total days: \(totalDaysFromBirth)")
+ */
