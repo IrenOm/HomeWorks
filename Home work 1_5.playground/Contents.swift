2b423d670070c4cfd882ff6068e590efa19b5a97
Irena Omelana <124605706+IrenOm@users.noreply.github.com>
2023-03-25 17:11:53 +0200

    H.w. 1_5

---
+++ Home work 1_5.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+/*
+ Ex1:
+ 1.1 Declare two variables of type Float and assign each a number with a fractional part - for example, 3.14 or 42.0
+ 1.2 Declare another variable of type Double and assign it the sum of two variables of type Float
+ 1.3 Print the result with print ()
+
+ */
+var nr1: Float = 3.14
+var nr2: Float = 42.0
+var nr3: Double = Double(nr1 + nr2)
+print(nr3)
+
+/*
+ Ex2:
+ 2.1 Create a variable numberOne and assign any integer value to it
+ 
+ 2.2 Create another integer variable numberTwo and assign it any value less than numberOne
+ 
+ 2.3 Set the new integer constant result to the result of dividing numberOne by numberTwo
+ 
+ 2.4 Assign the new integer constant remainder the remainder of numberOne divided by numberTwo
+ 
+ 2.5 Output to the console the message: "When dividing <...> by <...>, the result is <...>, the remainder is <...>".
+ 
+ */
+
+var numberOne: Int = 77
+var numberTwo: Int = 11
+var newConstantOne: Float = Float(numberOne / numberTwo)
+var newConstantTwo: Float = Float(numberOne % numberTwo)
+print("When dividing \(numberOne) by \(numberTwo), the result is \(newConstantOne), the remainder is \(newConstantTwo).")
+
+/*
+ Ex3:
+ You need to buy several new MacBook Pro, each cost 1000 EUR.
+ If you are buying 5 and more, with discount it will cost you 900 each! If you are buying 10 and more with discount it will cost you 850 each! Create if-else statements to check Conditions of buying in different amount!
+ print("new: \(qty) MacBook Pro with the price of: \(price) EUR, will cost you: \(totalSum) Eur")
+ */
+
+
+/*
+ Ex4:
+ Create String userInputAge and put value "33a" and convert to Int to make Fatal error: Unexpectedly found nil while unwrapping an Optional value!
+ Fix this Fatal error inside the if-else statements to print whenever this age can be converted to Int or not!
+ */
+
+
+/*
+ Ex5:
+ Calculate the number of years, months, days have passed from you birthday to current date.
+ print("Total years: \(totalYearsFromBirth) , total months: \(totalMonthFromBirth), total days: \(totalDaysFromBirth) have passed")
+ */
+
+/*
+ Ex6:
+ Use Exercise 5 monthOfBirth to calculate in which quarter of the year you were born.
+ Use switch case to print("I was born in the ... quarter")
+ */
